d1e1553c16e168b5fbdb6e1da96500bc97c89616
Trayan Iliev <ilievt@gmail.com>
2018-05-28 16:27:08 +0300

    flask_dynamic initial commit

--- flask_dynamic/flask-dynamic.py
+++ flask_dynamic/flask-dynamic.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask,redirect
+
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return redirect("http://localhost:5000/static/index.html", code=302)
+
+
+@app.route("/hello")
 def hello():
      return """
     <html>
