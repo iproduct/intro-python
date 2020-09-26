7b22567ee3cf6b8b2bcb936a3935f9059033b462
Trayan Iliev <office@iproduct.org>
2020-09-26 15:33:52 +0300

    singleton decorator example added

--- 06-sdp-oop/singleton.py
+++ 06-sdp-oop/singleton.py
@@ -9,31 +9,15 @@ def singleton(cls):
     @functools.wraps(cls.__new__)
     def singleton_new(cl, *args, **kwargs):
         if cl.__instance is None:
-            cl.__instance = cl.__new_original__(cl, *args, **kwargs)
-            cl.__instance.__init__ = cls.__init__
-            cl.__instance.__init__(cl.__instance, *args, **kwargs)
+            cl.__instance = cl.__new_original__(cl)
+            cl.__instance.__init_original__(*args, **kwargs)
         return cl.__instance
 
     cls.__new__ = singleton_new
-    return cls
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
 
-    # cls.__new_original__ = cls.__new__
-    #
-    # @functools.wraps(cls.__new__)
-    # def singleton_new(cls, *args, **kw):
-    #     it =  cls.__dict__.get('__it__')
-    #     if it is not None:
-    #         return it
-    #
-    #     cls.__it__ = it = cls.__new_original__(cls, *args, **kw)
-    #     it.__init_original__(*args, **kw)
-    #     return it
-    #
-    # cls.__new__ = singleton_new
-    # cls.__init_original__ = cls.__init__
-    # cls.__init__ = object.__init__
-    #
-    # return cls
+    return cls
 
 
 @singleton
@@ -45,11 +29,11 @@ class TheOne:
         return f'The one: {self.name}!'
 
 if __name__ == '__main__':
-    the_one1 = TheOne()
-    the_one2 = TheOne()
+    the_one1 = TheOne('Python')
+    the_one2 = TheOne('Go')
     print(id(the_one1))
     print(id(the_one2))
-    the_one2.name = 'John'
+    # the_one2.name = 'John'
     print(the_one1)
     print(the_one1 == the_one2)
     print(the_one1.__class__)
