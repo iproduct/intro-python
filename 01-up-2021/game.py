65cd0fd2f65248f20e41ef102c92bc9f4a58ccf1
Trayan Iliev <office@iproduct.org>
2021-02-08 18:40:55 +0200

    leasson 1 and 2 examples added

---
+++ 01-up-2021/game.py
@@ -0,0 +1,44 @@
+import random
+
+number_digits = 5
+digits = list(range(0, 10))
+
+def init():
+    global digits
+    random.shuffle(digits)
+    if(digits[0] == 0):
+        digits[0] = digits[9]
+    digits = digits[:number_digits]
+
+def get_bulls_cows(number):
+    global digits
+    num_str = str(number)
+    characters = list(num_str)
+    ans_digits = []
+    for ch in characters:
+        ans_digits.append(int(ch))
+    print(ans_digits)
+    bulls = 0
+    cows = 0
+    for i in range(number_digits):
+        if ans_digits[i] == digits[i]:
+            bulls += 1
+        elif ans_digits[i] in digits:
+            cows += 1
+    return bulls, cows
+
+
+if __name__ == '__main__':
+    init()
+    finished = False
+    print(digits)
+    while not finished:
+        answer = input("You number:")
+        ans_number = int(answer)
+        bulls, cows = get_bulls_cows(ans_number)
+        print(f"Bulls {bulls}, Cows: {cows}")
+        if bulls == number_digits:
+            print("Congratulations you win!")
+            finished = True
+
+    print("Good bye!")
---
+++ 01-up-2021/main.py
@@ -0,0 +1,28 @@
+# Press Shift+F10 to execute it or replace it with your code.
+
+def print_hi(name):
+    print(f'Hi, {name}!') # python 3.8+
+
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    if b == 0: # recursion bootm
+        return a
+    return gcd(b, a % b) # recursion step
+
+def calculate_pi(n):
+    percent = n /100
+    acc = 0
+    for i in range(n):
+        if i % percent == 0:
+            print(chr(0x25A9), sep="", end="")
+        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+    return acc
+
+
+if __name__ == '__main__':
+    # name = input("Enter your name:")
+    # print_hi(name)
+    # print(gcd(222, 234))
+    print("\n", calculate_pi(1000000000))
+
