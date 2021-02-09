0bc5993e01a80f859d29db0081ec6c6b08fb7f31
Trayan Iliev <office@iproduct.org>
2021-02-09 12:57:36 +0200

    eratosten primes demo added

--- 02-up-2021/primes.py
+++ 02-up-2021/primes.py
@@ -1,15 +1,18 @@
 max_number = 1000
 
 if __name__ == '__main__':
-    numbers = list(range(2,max_number + 1))
+    numbers = list(range(2, max_number + 1))
     next = 0
     while next < len(numbers):
         chosen = numbers[next]
+        # zero all multiples of chosen
         for i in range(2 * chosen, len(numbers) + 2, chosen):
             numbers[i - 2] = 0
         next += 1
+        # find first non-ziroed index
         while next < len(numbers) and not numbers[next] :
             next += 1
+
     print(numbers)
 
     for i in range(max_number + 1):
