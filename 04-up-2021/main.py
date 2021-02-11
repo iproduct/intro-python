30b7eb88baa90b379401fc7896ec72c491066745
Trayan Iliev <office@iproduct.org>
2021-02-11 12:34:51 +0200

    save to file added

--- 04-up-2021/main.py
+++ 04-up-2021/main.py
@@ -1,4 +1,4 @@
-
+import re
 
 def input_book():
     # input title
@@ -8,6 +8,9 @@ def input_book():
             break
         else:
             print("Грешка - заглавието трябва да бъде с дължина поне 3 символа.")
+
+    # input subtitle
+    subtitle = input("Въведете под-заглавие:")
 
     # input authors - comma separated
     while True:
@@ -41,12 +44,59 @@ def input_book():
         else:
             print("Грешка - тагове трябва да бъде с дължина поне 3 символа.")
 
+    # input publishing year
+    while True:
+        year = input("Въведете година на издаване:")
+        if re.match(r"[12][0-9]{3}", year):
+            break
+        else:
+            print("Грешка - въведете валидна година: напр. 1995")
+
+    # input language
+    while True:
+        language = input("1) Английски, 2) Български, 3) Немски, 4) Руски (<Enter> за Английски):")
+        if re.match(r"[1-4]?", language):
+            if language == "" or language == "1":
+                language = "English"
+            elif language == "2":
+                language = "Bulgarian"
+            elif language == "3":
+                language = "Deutsch"
+            elif language == "2":
+                language = "Russian"
+            break
+        else:
+            print("Грешка - въведете валидen избор")
 
     # return result book
-    return [title, authors, tags]
+    return [title, subtitle, authors, tags, year, language]
+
+
+def save_to_file(filename, library):
+    out = open(filename, "wt", encoding="utf-8")
+    for book in library:
+        write_book(out, book)
+
+    out.close()
+
+def write_book(out, book):
+    for i in range(len(book)):
+        value = book[i]
+        # print( type(value) )
+        if isinstance(value, str):
+            out.write(value)
+        elif isinstance(value, list):
+            list_str = ",".join(value)
+            out.write(f'"{list_str}"')
+        else:
+            continue
+        if i != len(book) - 1:
+            out.write(",")
 
 if __name__ == '__main__':
     library = []
     book = input_book()
     library.append(book)
     print(f"New book added: {library}")
+    # write to csv file
+    save_to_file("library.csv", library)
