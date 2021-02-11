d28f37ef36e2891b966f95469647bad6d52b2d74
Trayan Iliev <office@iproduct.org>
2021-02-11 09:27:31 +0200

    input book title and authors + tags

--- 04-up-2021/main.py
+++ 04-up-2021/main.py
@@ -1,6 +1,7 @@
 
 
 def input_book():
+    # input title
     while True:
         title = input("Въведете заглавие:")
         if len(title) >= 3:
@@ -8,6 +9,7 @@ def input_book():
         else:
             print("Грешка - заглавието трябва да бъде с дължина поне 3 символа.")
 
+    # input authors - comma separated
     while True:
         answer = input("Въведете автори (разделени със запетайки):")
         if len(answer) >= 3:
@@ -19,6 +21,9 @@ def input_book():
             break
         else:
             print("Грешка - автори трябва да бъде с дължина поне 3 символа.")
+
+    # input tags - comma separated, each tag should be atleast 2 characters long
+
     return [title, authors]
 
 if __name__ == '__main__':
