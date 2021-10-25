dbad8e0f977ad275a18c2833cef2439a603e5508
Trayan Iliev <office@iproduct.org>
2021-10-25 20:03:58 +0300

    first python demo commited

---
+++ 01-python-academy-intro-lab/main.py
@@ -0,0 +1,13 @@
+from other import other_func
+
+def print_hi(name):
+    """ prints hello message by name"""
+    print(f'Hi, {name} from Python 3.9!')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('Trayan')
+    other_func()
+
+
+
---
+++ 01-python-academy-intro-lab/other.py
@@ -0,0 +1,6 @@
+
+def other_func():
+    print(f"Other function in module: {__name__}")
+
+if __name__ == "__main__":
+    other_func()
