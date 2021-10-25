c6a41cd78335f48c3f2f2081b27fb5b196fc9dbb
Trayan Iliev <office@iproduct.org>
2021-10-25 21:17:35 +0300

    pi calculation with arbitrary precision

--- 01-python-academy-intro-lab/gcd.py
+++ 01-python-academy-intro-lab/gcd.py
@@ -1,12 +1,17 @@
+from mpmath import mp
+
+mp.dps = 500
+mp.pretty = True
 
 def calculate_pi(n):
     percent = n / 100
-    acc = 0
+    acc = mp.mpf(0)
     for i in range(n):
         if i % percent == 0:
-            print(chr(0x25A9), sep = "", end = "")
+            print(chr(0x25A9), sep="", end="")
         acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
     return acc
 
+
 if __name__ == '__main__':
-    print("\n", calculate_pi(100000000))
+    print("\n", calculate_pi(10000000))
