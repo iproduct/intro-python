33ff0793d5ac672318e64dc0e77c13152bc9817c
Trayan Iliev <office@iproduct.org>
2021-10-28 21:08:12 +0300

    experiments with basic datatypes - string, list, set, tuple, frozenset - splitting, joining, iteration, etc.

---
+++ 02-python-academy-basics-lab/experiments.py
@@ -0,0 +1,58 @@
+def f(x):
+    return x or "default"
+
+def remove_by_index(l, index):
+    """removes elemet with on index position from iterable l and returns the result as new object"""
+    return l[:index] + l[index + 1:]
+
+
+if __name__ == '__main__':
+    print(f("hello"))
+    print(" | ".join(["Python", "is", "easy"]))
+    print(f'Result is {f("hello")}')
+    print('Result is: %s' % f("hello"))
+    result = f("hello")
+    print('Result is: %s, len=%d' % (result, len(result)))
+    print('Result is: len = %(length)d, name=%(name)s' % {"name": result, "length": len(result)})
+    date_str = "12.07.1982"
+    date_result = date_str[:2] + date_str[3:5] + date_str[6:]
+    print(date_result)
+
+    sum = 0
+    for ch in date_str:
+        if ch != '.':
+            sum += int(ch)
+    print(f'Sum1 = {sum}')
+
+    digits_list = list(date_str)
+    print(digits_list)
+    sum = 0
+    for ch in digits_list:
+        if ch != '.':
+            sum += int(ch)
+    print(f'Sum2 = {sum}')
+
+    sum = 0
+    for ch in date_str:
+            sum += int(ch) if ch != '.' else 0
+    print(f'Sum3 = {sum}')
+
+    sum = 0
+    parts = date_str.split(sep=".")
+    print(parts)
+    for part in parts:
+        for digit in part:
+            # print(digit)
+            sum += int(digit)
+    print(f'Sum4 = {sum}')
+
+    # remove element by remove_by_index
+    digits_list = list(date_str)
+    print(remove_by_index(digits_list, 3)) # does not mutate original list
+    print(digits_list)
+
+    print(digits_list.pop(3)) # mutate original list
+    print(digits_list )
+
+    for d in set(date_str):
+        print(d)
