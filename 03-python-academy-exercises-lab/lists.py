dc1ce6a28dbc86cc61c6800c67720c02893eb738
Trayan Iliev <office@iproduct.org>
2021-11-04 19:33:03 +0200

    presentation 4 added

--- 03-python-academy-exercises-lab/lists.py
+++ 03-python-academy-exercises-lab/lists.py
@@ -23,7 +23,8 @@ if __name__ == '__main__':
     fruits = ["apple", "banana", "cherry", "kiwi", "mango", "orange", "papaya", "pineaple"] * 5
 
     newlist = [x for x in fruits if "a" in x]
-    newtuple = *(x for x in fruits if "a" in x),
+    newtuple = tuple(x for x in fruits if "a" in x)
+    newtuple2 = *(x for x in fruits if "a" in x),
     newset = {x  for x in fruits if "a" in x}
     newdict = {x : len(x) for x in fruits if "a" in x}
     generator = (x for x in fruits if "a" in x)
@@ -32,6 +33,7 @@ if __name__ == '__main__':
 
     print(newlist, "->", sys.getsizeof(newlist), "bytes")
     print(newtuple, "->", sys.getsizeof(newtuple), "bytes")
+    print(newtuple2, "->", sys.getsizeof(newtuple), "bytes")
     print(newset, "->", sys.getsizeof(newset), "bytes")
     print(newdict, "->", sys.getsizeof(newdict), "bytes")
     print(generator, "->", sys.getsizeof(generator), "bytes")
@@ -39,6 +41,7 @@ if __name__ == '__main__':
     print(range_len, "->", sys.getsizeof(range_len), "bytes")
 
     #UTF32
-    [print(chr(utf), end="") for utf in range(0x1F500, 0x1F7EA)]
-    print()
+    smilies = [chr(utf) for utf in range(0x1F5FF, 0x1F6EA)]
+    # for s in smilies:
+    #     print(f"{s:s} -> {ord(s):x}\n")
     print("\U0001F7EA" * 60)
--- 04-python-academy-files-lab/main.py
+++ 04-python-academy-files-lab/main.py
@@ -1,7 +1,25 @@
-
 def compare_str_ignore_case(s: str) -> str:
     return s.lower()
     # return str.lower(s)
+
+
+def book_by_title(book):
+    return book[1]
+
+
+def book_by_author(book):
+    return book[3]
+
+
+def book_by_price(book):
+    return book[6]
+
+
+def print_books(books):
+    for b in books:
+        print(
+            f'| {b[0]:>3d} | {b[1]:20.20s} | {b[2]:20.20s} | {b[3]:^15.15s} | {b[4]:^15.15s} | {b[5]:4d} | {str(b[6]) if len(b) > 6 else "-   ":>7.7s} |')
+
 
 if __name__ == '__main__':
     l = ['Orange', 'orange', 'banana', 'kiwi', 'mango', 'pineapple']
@@ -11,11 +29,35 @@ if __name__ == '__main__':
     books = [
         (1, "Learning Python", "", "Марк Лътз, Дейвид Асър", "O'Reily", 1999, 22.7),
         (2, "Think Python", "An Introduction to Software Design", "Алън Б. Дауни", "O'Reily", 2002, 9.4),
-        (3, "Python Crash Course, 2nd Edition", "A Hands-On, Project-Based Introduction to Programming", "Eric Matthes", "No Starch Press", 2014, 9.56),
+        (3, "Python Crash Course, 2nd Edition", "A Hands-On, Project-Based Introduction to Programming", "Eric Matthes",
+         "No Starch Press", 2014, 9.56),
         (4, "Python Pocket Reference", "Python in Your Pocket", "Mark Lutz", "O'Reily", 2002, 9.4),
-        (5, "Python for Data Analysis", "Data Wrangling with Pandas, NumPy, and IPython", "Wes Mckinney", "O'Reily", 2017, 30.75),
+        (5, "Python for Data Analysis", "Data Wrangling with Pandas, NumPy, and IPython", "Wes Mckinney", "O'Reily",
+         2017, 30.75),
         (6, "Python Cookbook", "Recipes for Mastering Python 3", "Браян К. Джоунс и Дейвид М. Баазли", "O'Reily", 2011,
          135.9)
     ]
 
+    # books_copy = books.copy()
+    # books_copy = list(books)
+    # books_copy = books[:]
+    books_copy = [book for book in books if book[3][0] < 'z']
 
+    books_copy.sort(key=book_by_price, reverse=True)
+    print_books(books)
+    print()
+    print_books(books_copy)
+
+    # tuples
+    t = (2, "Think Python", "An Introduction to Software Design", "Алън Б. Дауни", "O'Reily", 2002, 9.4)
+    newt = t + (["python", "introduction", "programming"],)
+    print(newt, ", ", t is newt)
+    print()
+    books_without_price = []
+    suma = 0
+    for book in books:
+        (*la, price) = book
+        books_without_price.append(la)
+        suma += price
+    print_books(books_without_price)
+    print(f"Total price: {suma}\n")
