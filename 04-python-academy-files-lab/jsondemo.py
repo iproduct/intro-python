b1efbfbd91351655b113191d086bb43f27b70ee7
Trayan Iliev <office@iproduct.org>
2021-11-08 20:05:09 +0200

    melting.py exercise sdded

--- 04-python-academy-files-lab/jsondemo.py
+++ 04-python-academy-files-lab/jsondemo.py
@@ -1,20 +1,25 @@
 import json
 
-x = {
-  "name": "John",
-  "age": 30,
-  "married": True,
-  "divorced": False,
-  "children": ("Ann","Billy"),
-  "pets": None,
-  "cars": [
-    {"model": "BMW 230", "mpg": 27.5},
-    {"model": "Ford Edge", "mpg": 24.1}
-  ]
-}
+if __name__ == "__main__":
+  person = {
+    "name": "John",
+    "age": 30,
+    "married": True,
+    "divorced": False,
+    "children": ("Ann","Billy"),
+    "pets": None,
+    "cars": [
+      {"model": "BMW 230", "mpg": 27.00},
+      {"model": "Ford Edge", "mpg": 24.1}
+    ]
+  }
 
-# convert into JSON:
-y = json.dumps(x, indent=4)
+  # convert into JSON:
+  with open("person.json", "wt", encoding="utf-8") as f:
+    json.dump(person, f, indent=4)
 
-# the result is a JSON string:
-print(y)
+  # the result is a JSON string:
+  with open("person.json", "rt", encoding="utf-8") as f:
+    result = json.load(f)
+    print(result)
+    print(type(result['age']))
