eb9b3cca2bec6a09396312d9b508b825a500f57c
Trayan Iliev <office@iproduct.org>
2021-11-25 20:20:28 +0200

    MyLibrary project id sequence generator externalized from repository

--- 07-classes-lab/property-decorator.py
+++ 07-classes-lab/property-decorator.py
@@ -5,20 +5,26 @@ class Celsius:
     def to_fahrenheit(self):
         return (self.temperature * 1.8) + 32
 
-    def get_temperature(self):
+    @property
+    def temperature(self):
         print(f"Getting value: {self.__temperature}")
         return self.__temperature # private attribute = encapsulation
 
-    def set_temperature(self, value):
+    @temperature.setter
+    def temperature(self, value):
         if value < -273:  # validation
             raise ValueError("Temperature below -273 is not possible")
         print(f"Setting value: {value}")
         self.__temperature = value  # private attribute = encapsulation
 
-    temperature = property(get_temperature, set_temperature)
+    @temperature.deleter
+    def temperature(self):
+      del self.__temperature
 
 if __name__ == '__main__':
     c = Celsius(300)
     # temp = c._Celsius__temperature;
     c.temperature -= 10;
     print(c.temperature)
+    del c.temperature
+    # print(c.temperature)
