eaafa66d721b71b474b079fa61366e7d8230c468
Trayan Iliev <office@iproduct.org>
2021-12-06 21:49:35 +0200

    vertical scrollbar added to treeview

--- 05-mylibrary-lab-next/presentation/show_books_view.py
+++ 05-mylibrary-lab-next/presentation/show_books_view.py
@@ -5,22 +5,35 @@ from dao.book_repository import BookRepository
 
 DEFAULT_COLUMN_WIDTH_PX = 100
 
-class  ShowBooksView(ttk.Frame):
+
+class ShowBooksView(ttk.Frame):
     def __init__(self, parent, label="", *args, entity_cls, books_repo: BookRepository):
-        super().__init__(parent, *args, padding="3 3 12 12")
+        super().__init__(parent, *args, padding="3 0 0 0")
         self.grid(column=0, row=0, sticky=(N, W, E, S))
 
         # define headings
         columns = tuple(entity_cls().__dict__.keys())
-        tree = ttk.Treeview(self, columns=columns, show='headings')
+        self.tree = ttk.Treeview(self, columns=columns, selectmode='browse', show='headings')
         for column in columns:
             print(column.title(), end=",")
-            tree.heading(column, text=column.title())
-            tree.column(column, width=DEFAULT_COLUMN_WIDTH_PX)
+            self.tree.heading(column, text=column.title())
+            self.tree.column(column, width=DEFAULT_COLUMN_WIDTH_PX)
 
-        tree.grid(row=0, column=0, sticky=NSEW)
+        self.tree.grid(row=0, column=0, sticky=NSEW)
+
+        # add a vertical scrollbar
+        vsb = ttk.Scrollbar(parent, orient="vertical", command=self.tree.yview)
+        vsb.grid(row=0, column=1, sticky=(N, W, S), padx=0)
+        self.tree.configure(yscrollcommand=vsb.set)
+
         self.update_idletasks()
-        parent.geometry(f"{tree.winfo_width() + 10}x{tree.winfo_height() + 200}")
+        parent.geometry(f"{self.tree.winfo_width() + 25}x{self.tree.winfo_height() + 200}")
+
+
+        item_ids = list(map(lambda book: self.tree.insert('', END, values=tuple(book.__dict__.values())), books_repo.find_all()))
+        item_ids = list(map(lambda book: self.tree.insert('', END, values=tuple(book.__dict__.values())), books_repo.find_all()))
+        item_ids = list(map(lambda book: self.tree.insert('', END, values=tuple(book.__dict__.values())), books_repo.find_all()))
+
 
         # # adding an item
         # tree.insert('', END, values=('John', 'Doe', 'john.doe@email.com'))
