3a217897d4d7e3716e8686509a770bd5b0191ed1
Trayan Iliev <office@iproduct.org>
2022-01-20 12:30:07 +0200

    Book example demo

--- 02-classes-library/entity/book.py
+++ 02-classes-library/entity/book.py
@@ -1,4 +1,30 @@
-
 class Book:
-    def __init__(self):
-        pass
+    next_id = 0  # unique id sequence
+
+    @classmethod
+    def get_next_id(cls):
+        cls.next_id += 1
+        return cls.next_id
+
+    def __init__(self, title=None, subtitle=None, authors=tuple(), isbn=None, publisher=None,
+                 year=None, price=None, genre=None, tags=tuple(), description=None):
+        # Book.next_id += 1
+        # self.__class__.next_id += 1
+        self.id = self.__class__.get_next_id()
+        self.title = title
+        self.subtitle = subtitle
+        self.authors = authors
+        self.isbn = isbn
+        self.publisher = publisher
+        self.year = year
+        self.price = price
+        self.genre = genre
+        self.tags = tags
+        self.description = description
+
+    def __str__(self):
+        return f"| {self.id:>3d} | {self.title:<20.20s} | {self.subtitle:<20.20s} | " \
+               f"{', '.join(self.authors):<20.20s} | {self.isbn:<10.10s} | " \
+               f"{self.publisher:<10.10s} | {self.price:<6.2f} | {self.year:<10.10s} | " \
+               f"{self.genre:<10.10s} | {', '.join(self.tags):<15.15s} |"
+
--- 02-classes-library/main.py
+++ 02-classes-library/main.py
@@ -1,3 +1,11 @@
+from entity.book import Book
 
 if __name__ == '__main__':
-    pass
+    b1 = Book("Think Python", "An Introduction to Software Design", ("Алън Б. Дауни",), "1491939362",
+              "O'Reily", 2002, 9.7, "Software engineering", ("python", "introduction", "programming"),
+              "If you want to learn how to program, working with Python is an excellent way to start. "
+              "This hands-on guide takes you through the language a step at a time, beginning with basic "
+              "programming concepts before moving on to functions, recursion, data structures, and "
+              "object-oriented design. This second edition and its supporting code have been updated "
+              "for Python 3.")
+    print(b1)
