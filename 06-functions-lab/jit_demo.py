fc7c5be5cb5121209fb4abb98783fa74a7550bd8
Trayan Iliev <office@iproduct.org>
2022-02-07 11:55:17 +0200

    function demos added

---
+++ 06-functions-lab/jit_demo.py
@@ -0,0 +1,33 @@
+from numba import jit
+import numpy as np
+import time
+
+@jit
+def f(x, y):
+    # A somewhat trivial example
+    return x + y
+
+x = np.arange(100).reshape(10, 10)
+
+@jit(nopython=True)
+def go_fast(a): # Function is compiled and runs in machine code
+    trace = 0.0
+    for i in range(a.shape[0]):
+        trace += np.tanh(a[i, i])
+    return a + trace
+
+
+if __name__ == '__main__':
+    print(f(1200, 3500))
+
+    # DO NOT REPORT THIS... COMPILATION TIME IS INCLUDED IN THE EXECUTION TIME!
+    start = time.time()
+    go_fast(x)
+    end = time.time()
+    print("Elapsed (with compilation) = %s" % (end - start))
+
+    # NOW THE FUNCTION IS COMPILED, RE-TIME IT EXECUTING FROM CACHE
+    start = time.time()
+    go_fast(x)
+    end = time.time()
+    print("Elapsed (after compilation) = %s" % (end - start))
