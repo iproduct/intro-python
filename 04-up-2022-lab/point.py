4bbc295e45dc986682e487f7fdda8485649cd3e3
Trayan Iliev <office@iproduct.org>
2022-02-13 10:01:24 +0200

    oop Point example added

--- 04-up-2022-lab/point.py
+++ 04-up-2022-lab/point.py
@@ -1,11 +1,51 @@
-class Point:
-    def __init__(self, x = 0, y = 0):
+from __future__ import annotations
+
+import math
+
+
+class Point(object):
+    """
+    Point models a 2D point in Decart plain
+    """
+    next_id = 0
+
+    @classmethod
+    def get_next_id(cls):
+        """ returns number of points created so far"""
+        cls.next_id += 1
+        return cls.next_id
+
+    def __init__(self, x=0, y=0):
+        """
+        Initialize point coordinates and id
+        :param x: x coordinate
+        :param y: y coordinate
+        """
+        self.id = self.get_next_id()
         self.x = x
         self.y = y
 
     def __str__(self):
-        return f"({self.x},{self.y})"
+        return f"({self.id}: {self.x},{self.y})"
+
+
+    def distance_to(self, other: Point) -> float:
+        """
+        returns the distance between self and other points
+        :param other: the point to calculate distance to
+        :return: the distance between two points
+        """
+        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
 
 if __name__ == "__main__":
-    p1 = Point(4,5)
+    p1 = Point(4, 5)
+    p2 = Point(0, 9)
+    p3 = Point(1, 1)
     print(p1)
+    print(p2)
+    print(p3)
+    print(p1.distance_to(p3))
+    print(p3.distance_to(p1))
+    print("Number of Pints created:", p1.next_id)
+    print("Number of Pints created:", p1.__class__.next_id)
+    print("Number of Pints created:", Point.next_id)
