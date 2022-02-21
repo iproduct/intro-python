2b491358ef0b1b10462cf86274f22a96e0295d78
Trayan Iliev <office@iproduct.org>
2022-02-21 11:04:21 +0200

    files word indexing demo debugged and completed

--- 06-file-search-engine/main.py
+++ 06-file-search-engine/main.py
@@ -1,23 +1,27 @@
 import os
+import sys
 
+import reducers
 from files.file_walker import walk_files_in_dir
 from filters.body_filter import body_filter
 from filters.html_content_filter import html_content_filter
 from reducers.word_count import word_count_reducer
 
 if __name__ == '__main__':
-    max_files = 4
+    print(sys.path)
+    print(dir(reducers.word_count))
+    max_files = 1
     count = 0
     for entry in walk_files_in_dir(r"D:\CoursePython\python-3.10.2-docs-html", recursive=True, regex=r"^.*\.html$"):
         if count >= max_files:
-            break;
+            break
         count += 1
         print("\n\nFILE:", entry.path)
         with open(entry.path, "rt", encoding="utf-8") as f:
             # word_counts = html_content_filter(body_filter(f))
             # for line in word_counts:
             #     print(line)
-            word_counts = word_count_reducer(html_content_filter(body_filter(f)))
+            word_counts = word_count_reducer(html_content_filter(body_filter(f)), max_keywords=20)
             for item in word_counts:
                 print(f"{item[0]:20s} -> {item[1]:3d}")
 
