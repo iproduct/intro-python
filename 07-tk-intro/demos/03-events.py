165443540cc461afdeac4ba99328c38e35c22835
Trayan Iliev <office@iproduct.org>
2022-02-28 12:48:00 +0200

    event handling demo added

---
+++ 07-tk-intro/demos/03-events.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry("800x400+300+100")
+    root.title("Feet to Meters Calculator")
+    mainframe = ttk.Frame(root, padding="50 30 50 30")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    l = ttk.Label(mainframe, text="Starting ...", width=100)
+    l.grid()
+
+    mainframe.bind('<Enter>', lambda e: l.configure(text="Moved mouse inside."))
+    mainframe.bind('<Leave>', lambda e: l.configure(text="Moved mouse outside."))
+    mainframe.bind('<ButtonPress-1>', lambda e: l.configure(text="Clicked left mouse button."))
+    mainframe.bind('<Button-3>', lambda e: l.configure(text="Clicked right mouse button."))
+    mainframe.bind('<Double-1>', lambda e: l.configure(text="Double clicked left mouse button."))
+    mainframe.bind('<Button3-Motion>', lambda e: l.configure(text=f"Dragged with right button to: {e.x}, {e.y}"))
+
+    root.mainloop()
