a537df2ce3ae393938c66e09c4a988fdaaaaa62d
Trayan Iliev <office@iproduct.org>
2022-02-28 21:09:32 +0200

    GCD problem setup added

---
+++ acad_2022_01_intro/primes.py
@@ -0,0 +1,30 @@
+
+
+def find_primes(n):
+    """
+    Returns all prime numbers between 2 and n
+    :param n: the upper bound
+    :return: sequence of all prime numbers less or equal to n
+    """
+    # numbers = [bool(x) for x in range(2, n+1)]
+    numbers = [True] * (n-1)
+    primes = []
+    i = 0
+    while i < n - 1:
+        while i < n - 1 and numbers[i] == False: # find first number that is True
+            i += 1
+        if i >= n-1:
+            break
+        prime = i + 2
+        primes.append(prime)
+        if prime * prime <= n:
+            j = i
+            while j < n-1:
+                numbers[j] = False
+                j += prime
+        i += 1
+    return primes
+
+
+if __name__ == '__main__':
+    print(find_primes(997))
