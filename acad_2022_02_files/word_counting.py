3a0417e91d4d74d33ac0399237194fe3c31c77d9
Trayan Iliev <office@iproduct.org>
2022-03-10 20:40:01 +0200

    stopwords added from nltk

--- acad_2022_02_files/word_counting.py
+++ acad_2022_02_files/word_counting.py
@@ -1,11 +1,27 @@
 import re
 import nltk
 
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
+#
+# nltk.download('stopwords')
+# stops = set(stopwords.words('english'))
 
-nltk.download('stopwords')
-stops = set(stopwords.words('english'))
-
+stops= {'had', 'him', "isn't", "shan't", 'yourself', 'will', 'couldn', 'hadn', "shouldn't", 'his',
+        'below', 'between', 'other', 'd', 'been', 'its', 'too', 're', "you've", 'they', 'against',
+        'these', "don't", 'her', 'won', 'o', 'again', "it's", 'aren', "didn't", 'some', 'we', 'a',
+        'through', 'doesn', "hasn't", 'or', 'me', 'an', 'herself', 'all', 'your', 'by', 'ourselves',
+        "wasn't", 'did', "wouldn't", 'once', 'she', 'is', 'from', 'it', 'how', 'what', 'll', 'mightn',
+        'for', 've', 'now', 'most', 'up', 'hasn', "you're", 'you', 'that', 'on', 'so', 'who', "mightn't",
+        'not', 'during', 'further', "haven't", 'them', 'than', "should've", 'their', 'and', 'didn', 'just',
+        'itself', 'there', "aren't", "she's", 'each', 'own', 'only', 'which', 'above', 'as', 'needn',
+        'where', 'yours', 'should', 'until', 'the', 'he', 's', 'be', 'off', 'no', "won't", 'ma', 'theirs',
+        "needn't", 'out', 'after', "couldn't", 'were', 'down', 'at', 'themselves', 'of', 'in', 'any', 'about',
+         'i', 'then', 'before', 'do', 'with', 'few', 'ain', 'mustn', "that'll", 'nor', 'ours', 'm', 'same',
+        'wouldn', 'this', 'here', 'hers', 'whom', 'under', 'if', 'myself', 'because', 'was', 'doing', 'don',
+        'having', 'being', 'isn', 'those', 'am', 'himself', 'both', 'more', "doesn't", 'haven', 'why', 'have',
+        'but', 'to', "you'll", 'does', 'wasn', 'while', 'my', 'are', 'our', "you'd", 'shouldn', "mustn't",
+        'weren', 'such', 'shan', 'yourselves', 'very', "weren't", 'into', 'when', 't', 'has', 'y', "hadn't",
+        'can', 'over'}
 print(stops)
 
 def index_file(file_name: str, max_words: int = 20) -> list[tuple[str, int]]:
