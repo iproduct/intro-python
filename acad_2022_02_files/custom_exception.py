3e1a16a8a3d8eed1087f80a18088a34466a97e5d
Trayan Iliev <office@iproduct.org>
2022-03-17 21:13:22 +0200

    file exception handling - traceback custom walking and FrameSummary printing

--- acad_2022_02_files/custom_exception.py
+++ acad_2022_02_files/custom_exception.py
@@ -21,11 +21,13 @@ if __name__ == "__main__":
         try:
             erroneous()
         except Exception as ex:
-            raise MyException("Risen from except clause: " + str(ex)) from ex
-    except MyException as ex:
-        exinfo = sys.exc_info()
-        print("Catched:", ex, ", Exc. info:", exinfo, ":")
-        traceback.print_tb(exinfo[2], file=sys.stdout)
+            raise #MyException("Risen from except clause: " + str(ex)) from ex
+    except Exception as ex:
+        ex_info = sys.exc_info()
+        print("Catched:", ex, ", Exc. info:", ex_info, ":")
+        traceback.print_tb(ex_info[2], file=sys.stdout)
+        summary = traceback.StackSummary.extract(traceback.walk_tb(ex_info[2]))
+        print(">>>", summary)
 
 
     print("Program contounes normally.")
