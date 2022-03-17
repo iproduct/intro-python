e9e5f52c3be523404403ee3f31c6a8262aafe38a
Trayan Iliev <office@iproduct.org>
2022-03-17 19:43:55 +0200

    json lightweight data persistence

--- acad_2022_02_files/json_demo.py
+++ acad_2022_02_files/json_demo.py
@@ -3,7 +3,7 @@ import json
 books = [
     (1, "Learning Python", "", "Марк Лътз, Дейвид Асър", "O'Reily", 1997, 22.7),
     (2, "Think Python", "An Introduction to Software Design", "Алън Б. Дауни", "O'Reily", 2002, 9.4),
-    (3, "Python Cookbook", "Recipes for Mastering Python 3", "Браян К. Джоунс и Дейвид М. Баазли", "O'Reily", 2011,
+    (3, "Python Cookbook", "Recipes for Mastering Python 3", "Браян К. Джоунс, Дейвид М. Баазли", "O'Reily", 2011,
      62.6),
     (4, "Learning Python 2 ed.", "", "Марк Лътз, Дейвид Асър", "O'Reily", 1998, 22.7),
     (5, "Think Python 2 ed.", "An Introduction to Software Design", "Алън Б. Дауни", "O'Reily", 2002, 19.4),
@@ -11,8 +11,7 @@ books = [
      56.7),
     (7, "Learning Python 3 ed.", "", "Марк Лътз, Дейвид Асър", "O'Reily", 2001, 28.9),
     (8, "Think Python 3 ed.", "An Introduction to Software Design", "Алън Б. Дауни", "O'Reily", 2002, 23.4),
-    (
-    9, "Python Cookbook 3 ed.", "Recipes for Mastering Python 3", "Браян К. Джоунс и Дейвид М. Баазли", "O'Reily", 2011,
+    (9, "Python Cookbook 3 ed.", "Recipes for Mastering Python 3", "Браян К. Джоунс, Дейвид М. Баазли", "O'Reily", 2011,
     135.9)
 ]
 
