10b46ff3578efd3704ea616d295ab15184a63ac7
Trayan Iliev <office@iproduct.org>
2022-03-17 13:06:54 +0200

    pandas03 demo added

---
+++ 10-numpy-pandas-lab/pandas03.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    pd.options.display.max_rows = 9999
+    workouts = pd.read_csv("data/workouts.csv")
+    # workouts.to_json("data/workouts.json")
+    # workouts2 = pd.read_json("data/workouts.json")
+
+    # impute missing values
+    median_calories = workouts["Calories"].median()
+    print(f'Mean calories: {median_calories}')
+    workouts["Calories"].fillna(median_calories, inplace=True)
+
+    print(workouts)
+    print(workouts.info())
+    print(workouts.describe())
+
+    # calculate correlation
+    print(workouts.corr())
+
+    # plot charts
+    workouts.plot()
+    plt.savefig("workouts.png", format="png")
+    plt.show()
+
