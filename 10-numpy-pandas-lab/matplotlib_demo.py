f219540a45c9147bd3a8aa945d691cbcc03d88dc
Trayan Iliev <office@iproduct.org>
2022-03-17 09:17:35 +0200

    matplotlib demo added

---
+++ 10-numpy-pandas-lab/matplotlib_demo.py
@@ -0,0 +1,13 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([55, 15, 25, 5])
+
+plt.pie(y)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+# plt.savefig(sys.stdout.buffer)
+# sys.stdout.flush()
