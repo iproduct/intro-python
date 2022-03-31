301e0a04f398090f86ab210b5a623057d07bfcbc
Trayan Iliev <office@iproduct.org>
2022-03-31 21:21:41 +0300

    loading added

--- acad_2022_04_classes/dao/json_repository.py
+++ acad_2022_04_classes/dao/json_repository.py
@@ -4,13 +4,21 @@ from dao.repository import Repository
 
 
 class JsonRepository(Repository):
-    def __init__(self, idGenerator, db_filename):
+    def __init__(self, idGenerator, db_filename, entity_class):
         super().__init__(idGenerator)
+        self.entity_class = entity_class
         self.db_filename = db_filename
 
     def save(self):
         with open(self.db_filename, 'wt', encoding='utf-8') as f:
             json.dump(list(self.find_all()), f, indent=4, default=dumper)
+
+    def load(self):
+        self.clear()
+        with open(self.db_filename, "rt", encoding="utf-8") as f:
+            users = json.load(f, object_hook=object_hook(self.entity_class))  # IIFE
+            for user in users:
+                self.create(user)
 
 
 # Helpers
@@ -21,3 +29,10 @@ def dumper(obj):
         return obj.__dict__
 
 
+def object_hook(entity_class):  # HOF
+    def obj_hook(jsdict):
+        obj = entity_class()
+        obj.__dict__ = jsdict
+        return obj
+
+    return obj_hook
