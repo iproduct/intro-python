ba7fabaa9020e61fb96bb4fb70353f3f430d3998
Trayan Iliev <office@iproduct.org>
2022-09-30 14:13:10 +0300

    abstract tree added

---
+++ sdp-2022-04-alorithms-datastructs/abstract_tree.py
@@ -0,0 +1,37 @@
+from typing import Callable
+
+
+class AbstractTree:
+    def size(self):
+        raise NotImplemented('Method not implemented.')
+    def values_dfs_preorder(self):
+        raise NotImplemented('Method not implemented.')
+    def values_dfs_postorder(self):
+        raise NotImplemented('Method not implemented.')
+    def nodes_dfs_preorder(self):
+        raise NotImplemented('Method not implemented.')
+    def nodes_dfs_postrder(self):
+        raise NotImplemented('Method not implemented.')
+    def nodes_bfs_preorder(self):
+        raise NotImplemented('Method not implemented.')
+    def root(self):
+        raise NotImplemented('Method not implemented.')
+    def parent(self, p):
+        raise NotImplemented('Method not implemented.')
+    def children(self, p):
+        raise NotImplemented('Method not implemented.')
+    def is_internal(self, node):
+        raise NotImplemented('Method not implemented.')
+    def is_root(self, node):
+        raise NotImplemented('Method not implemented.')
+    def swap_elements(self, p, q):
+        raise NotImplemented('Method not implemented.')
+    def replace_element(self, p, o):
+        raise NotImplemented('Method not implemented.')
+    def find(self, value):
+        raise NotImplemented('Method not implemented.')
+    def find_first(self, value):
+        raise NotImplemented('Method not implemented.')
+    def find_by_predicate(self, predicate, start_from = None):
+        raise NotImplemented('Method not implemented.')
+
