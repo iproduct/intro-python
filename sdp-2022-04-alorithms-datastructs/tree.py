3035d931361655c1d1088f033ed9bfa7c55f494d
Trayan Iliev <office@iproduct.org>
2022-09-30 14:48:31 +0300

    tree dfs added

--- sdp-2022-04-alorithms-datastructs/tree.py
+++ sdp-2022-04-alorithms-datastructs/tree.py
@@ -1,9 +1,11 @@
+from abstract_tree import AbstractTree
+from typing import Callable, Iterable
 
 class TreeNode:
-    def __init__(self, data = Node, parent= None, children=()):
+    def __init__(self, data = None, parent= None, children=None):
         self.data = data
         self.parent = parent
-        self.children = children
+        self.children = children if children is not None else []
 
     def __str__(self):
         result = str(self.data)
@@ -16,6 +18,12 @@ class TreeNode:
             result += ')'
         return  result
 
+    def __len__(self):
+        count = 1
+        for child in self.children:
+            count += len(child)
+        return count
+
     def add_child(self, child_node):
         child_node.parent = self
         self.children.append(child_node)
@@ -24,19 +32,31 @@ class TreeNode:
         for child in children:
             self.add_child(child)
 
+
+
 class Tree(AbstractTree):
     def __init__(self, root=None):
         self.root = root
-        self.size = 0
+        self.size = 0 if root is None else len(root)
         
     def __str__(self):
         return str(self.root)
     
     def is_empty(self):
-        return size == 0
+        return self.size == 0
 
     def size(self):
-        return size
+        return self.size
+
+    def add_child(self, parent_node: TreeNode, child_node: TreeNode):
+        parent_node.add_child(child_node)
+        self.size += len(child_node)
+
+    def add_children(self,  parent_node: TreeNode, *children):
+        parent_node.add_children(*children)
+        for child in children:
+            self.size += len(child)
+
 
     def values_dfs_postorder(self):
         for node in self.nodes_dfs_preorder():
@@ -44,7 +64,7 @@ class Tree(AbstractTree):
 
     def nodes_dfs_preorder(self):
         stack = Stack()
-        if root == None: return
+        if self.root is None: return
         stack.push(self.root)
         while not stack.is_empty():
             node = stack.pop()
@@ -96,6 +116,6 @@ if __name__ == '__main__':
     if node_c != None:
         t.add_children(node_c, TreeNode('E'), TreeNode('F'), TreeNode('G'))
     print(f'Tree:{t}, of size:{t.size}')
-    for node in t.nodes_dfs_preorder():
-        print(node.data, end=', ')
-    print()
+    # for node in t.nodes_dfs_preorder():
+    #     print(node.data, end=', ')
+    # print()
