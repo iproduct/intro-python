3673930f2d6c969ccb1d580a9f3aa14cbeedac92
Trayan Iliev <office@iproduct.org>
2023-02-07 15:18:41 +0200

    loading product prices from CSV file complete

--- fmi-2023-up-01/pazaruvane.py
+++ fmi-2023-up-01/pazaruvane.py
@@ -24,13 +24,24 @@ def input_product_list() -> list[product_type]:
 
 def stringify_product(product: product_type) -> str:
     return f'{product[0]:<15s} -> {product[1]:7.2f}\n'
+
 def save_product_list(products: list[product_type]):
     product_strings = map(stringify_product, products)
     with open('shoping_list.txt', 'wt', encoding='utf-8') as f:
         f.writelines(product_strings)
 
+def load_product_prices(filename: str) -> list[product_type]:
+    results = []
+    with open(filename, 'rt', encoding='utf-8') as f:
+        for line in f:
+            name_and_price_list = line.strip().split(',')
+            results.append((name_and_price_list[0].strip(), float(name_and_price_list[1].strip())))
+    return results
 
 
 if __name__ == '__main__':
+    product_prices = load_product_prices('product_prices.csv')
+    print(product_prices)
     products = input_product_list()
     save_product_list(products)
+    print('Shopping saved succesfully')
