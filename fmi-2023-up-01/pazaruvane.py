fca42f307a438e3fb004212a23dd595da43efd16
Trayan Iliev <office@iproduct.org>
2023-02-07 14:50:55 +0200

    shopping list problem solved

---
+++ fmi-2023-up-01/pazaruvane.py
@@ -0,0 +1,36 @@
+product_type = tuple[str, float]
+def input_product() -> product_type:
+    """
+    Inputs a product from command line
+    :return: a tuple of product name and quantity
+    """
+    product_name = input('Product name (empty for end):').strip()
+    if len(product_name) == 0:
+        return None
+    qty = float(input('Quantity:').strip())
+    return (product_name, qty)
+
+def input_product_list() -> list[product_type]:
+    products = []
+    finished = False
+    while not finished:
+        product = input_product()
+        if product is None:
+            finished = True
+        else:
+            products.append(product)
+    return products
+
+
+def stringify_product(product: product_type) -> str:
+    return f'{product[0]:<15s} -> {product[1]:7.2f}\n'
+def save_product_list(products: list[product_type]):
+    product_strings = map(stringify_product, products)
+    with open('shoping_list.txt', 'wt', encoding='utf-8') as f:
+        f.writelines(product_strings)
+
+
+
+if __name__ == '__main__':
+    products = input_product_list()
+    save_product_list(products)
