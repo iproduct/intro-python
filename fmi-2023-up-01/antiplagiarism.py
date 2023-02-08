2d9605f91133419583e31481fd84efb983996ed5
Trayan Iliev <office@iproduct.org>
2023-02-08 12:37:17 +0200

    word counts sorted

--- fmi-2023-up-01/antiplagiarism.py
+++ fmi-2023-up-01/antiplagiarism.py
@@ -1,3 +1,5 @@
+import re
+
 def strip_comments(lines: list[str]) -> list[str]:
     """
     returns lines with stripped comments (starting with #) and empty lines
@@ -18,6 +20,23 @@ def strip_comments(lines: list[str]) -> list[str]:
     return results
 
 
+def count_words(lines: list[str], word_counts: dict[str, int]):
+    for line in lines:
+        words = re.split('\W+', line)
+        for word in words:
+            word = word.strip()
+            if len(word) > 1:
+                word_counts[word] = word_counts.get(word, 0) + 1
+
+def get_count(word: tuple[str, int]) -> int:
+    return (word[1], word[0])
+
+def sort_words(word_counts: dict[str, int]) -> list[tuple[str, int]]:
+    wc_list = list(word_counts.items())
+    wc_list.sort(key=get_count, reverse=True)
+    return wc_list
+
+
 if __name__ == '__main__':
     # 1
     file1 = input('File name 1:')
@@ -32,5 +51,15 @@ if __name__ == '__main__':
     #3
     no_comments1 = strip_comments(lines1)
     no_comments2 = strip_comments(lines2)
-    print(no_comments1)
-    print(no_comments2)
+
+    #4
+    wc1 = dict()
+    count_words(no_comments1, wc1)
+    wc2 = dict()
+    count_words(no_comments2, wc2)
+
+    #5
+    wc_sorted1 = sort_words(wc1)
+    wc_sorted2 = sort_words(wc2)
+    print(wc_sorted1)
+    print(wc_sorted2)
