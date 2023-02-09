30bd8f68912f83198c745931bf8332a6baca1a1b
Trayan Iliev <office@iproduct.org>
2023-02-09 12:02:14 +0200

    contacts demo - edit function added

--- fmi-2023-up-01/contatcs.py
+++ fmi-2023-up-01/contatcs.py
@@ -2,14 +2,18 @@ from typing import Callable
 
 def print_contacts():
     print(f'\nAll Contacts:')
+    get_print_contacts()
+
+def get_print_contacts():
     with open('contacts.csv', 'rt', encoding='utf-8') as f:
         lines = f.readlines()
-    for line in lines:
+    for index, line in enumerate(lines):
         line = line.strip()
         if len(line) == 0:
             continue
         parts = line.split(',')
-        print(f'{parts[0].strip():20} | {parts[1].strip():15}')
+        print(f'{index + 1} | {parts[0].strip():20} | {parts[1].strip():15}')
+    return lines
 
 def input_contact():
     print(f'\nAdd New Contact:')
@@ -19,17 +23,29 @@ def input_contact():
     with open('contacts.csv', 'at', encoding='utf-8') as f:
         f.write(line)
 
-
 def delete_contact():
     print(f'\nDelete Contact:')
-    with open('contacts.csv', 'rt', encoding='utf-8') as f:
-        lines = f.readlines()
-    for index, line in enumerate(lines):
-        line = line.strip()
-        parts = line.split(',')
-        print(f'{index + 1} | {parts[0].strip():20} | {parts[1].strip():15}')
-    selected = int(input('Select contact to delete:'))
+    lines = get_print_contacts()
+    selected = int(input('Select contact to DELETE:'))
     lines.pop(selected - 1)
+    with open('contacts.csv', 'wt', encoding='utf-8') as f:
+        f.writelines(lines)
+
+def edit_contact():
+    print(f'\nEdit Contact:')
+    lines = get_print_contacts()
+    selected = int(input('Select contact to EDIT:'))
+    line = lines[selected - 1]
+    parts = line.split(',')
+    name = parts[0].strip()
+    phone  = parts[1].strip()
+    new_name = input(f'Name [{name} - <Enter to keep>]:').strip()
+    if len(new_name) == 0:
+        new_name = name
+    new_phone = input(f'Phone [{phone} - <Enter to keep>]:').strip()
+    if len(new_phone) == 0:
+        new_phone = phone
+    lines[selected - 1] = f'{new_name},{new_phone}\n'
     with open('contacts.csv', 'wt', encoding='utf-8') as f:
         f.writelines(lines)
 
