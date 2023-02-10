a66567515237dca3bedd269118da055ede02fcce
Trayan Iliev <office@iproduct.org>
2023-02-10 17:05:24 +0200

    ContactRepository class implemented

---
+++ fmi-2023-up-02-oop/contact_repository.py
@@ -0,0 +1,31 @@
+from typing import Iterable
+from contact import Contact
+from uuid import uuid4
+
+
+class ContactRepository:
+    def __init__(self):
+        self.contacts = dict()
+
+    def find_all(self) -> Iterable[Contact]:
+        return self.contacts.values()
+
+    def find_by_id(self, id: str):
+        return self.contacts[id]
+
+    def create(self, contact: Contact):
+        contact.id = uuid4()
+        self.contacts[contact.id] = contact
+        return contact
+
+    def update(self, contact):
+        self.contacts[contact.id] = contact
+        return contact
+
+    def delete_by_id(self, id: str):
+        deleted = self.contacts[id]
+        del(self.contacts[id])
+        return deleted
+
+    def count(self):
+        return len(self.contacts)
