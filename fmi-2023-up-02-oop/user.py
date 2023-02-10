26c399326f1eb5e21dd367478f6b827c90e5e545
Trayan Iliev <office@iproduct.org>
2023-02-10 16:28:11 +0200

    multiple inheritance demo complete

--- fmi-2023-up-02-oop/user.py
+++ fmi-2023-up-02-oop/user.py
@@ -1,26 +1,32 @@
+from addressable import Addressable
 from person import Person
 from student import Student
 
 ADMIN = "Admin"
 USER = "User"
 
-class User(Person, Student):
-    def __init__(self, id, name, age, username, password, role=USER):
+class User(Person, Addressable):
+    def __init__(self, id, name, age, username, password, addr, role=USER):
         super().__init__(id, name, age)
+        Addressable.__init__(self, addr.address, addr.street, addr.city, addr.country)
         self.username = username
         self.password = password
         self.role = role
 
     def __str__(self):
-        return f'{super().__str__()}, Username: {self.username}, Pass: {self.password}, Role: {self.role}'
+        return f'{Person.__str__(self)}, Username: {self.username}, Pass: {self.password}, Role: {self.role}, Address:{Addressable.__repr__(self)}'
 
 if __name__ == '__main__':
     p1 = Person(1, 'Ivan Petrov', 35)
-    print(p1, type(p1))
-    u2 = User(2, 'John Doe', 42, 'john', 'john123', ADMIN)
-    u3 = User(3, 'Jane Doe', 29, 'jane', 'jane123')
-    print(u2, type(u2))
+    print(p1)
+    u2 = User(2, 'John Doe', 42, 'john', 'john123', Addressable('1000', 'J. Bouchier', 'Sofia'), ADMIN)
+    u3 = User(3, 'Jane Doe', 29, 'jane', 'jane123', Addressable('2000', 'J. Bouchier', 'Sofia'))
+    print(u2)
+    print(u2.city)
+    print(u3)
     print(f'Is u2 User?: {isinstance(u2, User)}')
     print(f'Is u2 Person?: {isinstance(u2, Person)}')
     print(f'Is u2 object?: {isinstance(u2, object)}')
     print(f'Is u2 dict?: {isinstance(u2, dict)}')
+    print(u2.get_label())
+    print(u2.say_hi())
