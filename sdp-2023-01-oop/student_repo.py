2c36573e8691c6ad2a3a43e30868fc4bd4e765dd
Trayan Iliev <office@iproduct.org>
2023-09-22 16:02:15 +0300

    StudentRepository add student method added

--- sdp-2023-01-oop/student_repo.py
+++ sdp-2023-01-oop/student_repo.py
@@ -4,27 +4,39 @@ from student import Student, print_students
 class StudentRepository:
     def __init__(self, initial_students_list=None):
         students_list = [] if initial_students_list is None else initial_students_list
-        self.students = {s.id: s for s in students_list}
+        self.students: dict[Student] = {s.id: s for s in students_list}
 
     def find_all(self):
         return list(self.students.values())
 
-    def find_by_id(self, id):
+    def find_by_id(self, id: str) -> list[Student]:
         return self.students[id]
-    def find_by_fn(self, fn):
+
+    def find_by_fn(self, fn: str) -> list[Student]:
         return [s for s in self.students.values() if s.fn == str(fn)]
 
+    def find_by_name(self, name_part: str) -> list[Student]:
+        return [s for s in self.students.values() if name_part.lower() in s.name.lower()]
+
+    def add(self, student: Student):
+        self.students[student.id] = student
 
 if __name__ == '__main__':
     students = [
         Student('65300', 'Georgi Petrov', '23.07.1999'),
         Student('65301', 'Hristina Dimitrova', '07.05.2005', 2),
         Student('65305', 'Ivan Genov', '21.09.1982', 2),
+        Student('65306', 'Georgi Genov', '21.09.1982', 3),
+        Student('65307', 'Temenuzka Georgieva', '21.09.1982', 4),
     ]
     repo = StudentRepository(students)
+    repo.add(Student('68359', 'Atanas Petrov', '18.11.1979', 4))
+
     print_students(repo.find_all())
 
     print()
     print(repo.find_by_id(2))
     print()
-    print(repo.find_by_fn(65305))
+    print(repo.find_by_fn('65305'))
+    print()
+    print_students(repo.find_by_name('Georgi'))
