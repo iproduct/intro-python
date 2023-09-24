e61f476cf38edc829ef9a87a475840470e5274da
Trayan Iliev <office@iproduct.org>
2023-09-24 16:59:25 +0300

    opencv image demo added

--- sdp-2023-03-import-packages/opencv-capture.py
+++ sdp-2023-03-import-packages/opencv-capture.py
@@ -1,5 +1,4 @@
-import cv2 as cv
-
+import cv2 as lena.bmp
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -16,6 +15,7 @@ while True:
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Display the resulting frame
     cv.imshow('frame', frame)
+    cv.imshow('gray', gray)
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
