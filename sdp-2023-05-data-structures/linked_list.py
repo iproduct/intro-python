b166683d2b222e9b51b397bf9f8615d67839aecc
Trayan Iliev <office@iproduct.org>
2023-09-26 17:30:52 +0300

    linked list implemented

---
+++ sdp-2023-05-data-structures/linked_list.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def __iter__(self):
+        node = self.first
+        while node is not None:
+            yield node.value
+            node = node.next
+
+    def insert(self, value, index=0):
+        if index < 0 or index > self.length:
+            raise IndexError(f'Invalid index = {index}, should be between 0 and {self.length}')
+        if index == 0:
+            new_node = Node(value, None, self.first)
+            if self.first is not None:
+                self.first.prev = new_node
+            self.first = new_node
+            if self.last is None:
+                self.last = new_node
+        elif index == self.length:
+            new_node = Node(value, self.last, None)
+            self.last.next = new_node
+            self.last = new_node
+        else:
+            node = self._nth_node(index)
+            new_node = Node(value, node.prev, node)
+            node.prev.next = new_node
+            node.prev = new_node
+        self.length += 1
+
+    def _nth_node(self, n):
+        i = 0
+        node = self.first
+        while i < n and node is not None:
+            node = node.next
+            i += 1
+        return node
+
+    def append(self, value):
+        self.insert(value, self.length)
+
+    def pop(self, index=None):
+        if index is None:
+            index = self.length - 1
+        if index < 0 or index >= self.length:
+            raise IndexError(f'Invalid index = {index}, should be between 0 and {self.length - 1}')
+        if index == 0:
+            result = self.first.value
+            self.first = self.first.next
+            self.first.prev = None
+            if self.first is None:
+                self.last = None
+        elif index == self.length - 1:
+            result = self.first.value
+            self.last = self.last.prev
+            self.last.next = None
+            if self.last is None:
+                self.first = None
+        else:
+            node = self._nth_node(index)
+            node.prev.next = node.next
+            node.next.prev = node.prev
+        self.length -= 1
+
+    def peek(self, index=None):
+        if index is None:
+            index = self.length - 1
+        if index < 0 or index >= self.length:
+            raise IndexError(f'Invalid index = {index}, should be between 0 and {self.length - 1}')
+        return self._nth_node(index)
