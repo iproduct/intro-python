0e1785abb9673935bf9d74287edcadb6c1d05243
Trayan Iliev <office@iproduct.org>
2024-02-07 12:10:26 +0200

    labyrinth completed

--- fmi-2024-up-01-intro-python/labyrinth.py
+++ fmi-2024-up-01-intro-python/labyrinth.py
@@ -1,33 +1,46 @@
 
 lab = (
-    (0, 0, 0, 1, 0, 1)
-    (1, 0, 0, 1, 0, 0)
-    (0, 1, 0, 1, 0, 1)
-    (0, 0, 0, 1, 0, 1)
-    (1, 0, 1, 1, 0, 1)
-    (0, 0, 0, 0, 0, 1)
+    (0, 0, 0, 1, 0, 1),
+    (1, 0, 0, 1, 0, 0),
+    (0, 1, 0, 1, 0, 1),
+    (0, 0, 0, 1, 0, 1),
+    (1, 0, 1, 1, 0, 1),
+    (0, 0, 0, 0, 0, 1),
 )
 
-start =(1, 1)
-goal = (6, 2)
+start =(0, 0)
+goal = (5, 1)
 
 
 def find_neighbours(start, lab):
     x, y = start
     results = []
     lenY = len(lab)
-    lenX = len(lab(0))
-    if x > 1 and lab[y-1, x-2] == 0:
+    lenX = len(lab[0])
+    if x > 1 and lab[y][x-1] == 0:
        results.append((x-1, y))
-    if x < lenX and lab[y-1, x] == 0:
+    if x < lenX-1 and lab[y][x+1] == 0:
        results.append((x+1, y))
-    if y > 1 and lab[y-2, x-1]:
+    if y > 1 and lab[y-1][x] == 0:
        results.append((x, y-1))
-    if y < lenY and lab[y, x-1]:
+    if y < lenY-1 and lab[y+1][x] == 0:
        results.append((x, y + 1))
     return results
 
-def find_path(start, goal):
+def find_path(start, goal, lab, visited):
     if start == goal:
         return [goal]
-    neighbours = find_neighbours(start)
+    neighbours = find_neighbours(start, lab)
+    for neighbour in neighbours:
+        if neighbour in visited:
+            continue
+        visited.append(neighbour)
+        path = find_path(neighbour, goal, lab, visited)
+        if path is not None:
+            return [start] + path
+    return None
+if __name__ == "__main__":
+    n = find_neighbours((2,3), lab)
+    path = find_path(start, goal, lab, [])
+    print(path)
+    
