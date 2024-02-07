ceafd7d9b6e7972d06603e7e5b50b99be265875f
Trayan Iliev <office@iproduct.org>
2024-02-07 12:18:00 +0200

    labyrinth demo completed

--- fmi-2024-up-01-intro-python/labyrinth.py
+++ fmi-2024-up-01-intro-python/labyrinth.py
@@ -37,7 +37,8 @@ def find_path(start, goal, lab, visited):
         visited.append(neighbour)
         path = find_path(neighbour, goal, lab, visited)
         if path is not None:
-            return [start] + path
+            path.insert(0, start)
+            return path
     return None
 if __name__ == "__main__":
     n = find_neighbours((2,3), lab)
