4d02770b08939886fb96d04f20f46081229a6953
Trayan Iliev <office@iproduct.org>
2024-02-08 11:45:06 +0200

    contacts demo - formatted contacts table

--- fmi-2024-up-01-intro-python/contacts.py
+++ fmi-2024-up-01-intro-python/contacts.py
@@ -2,6 +2,7 @@ import json
 
 sample_contacts = [
     {
+        "id": 1,
         "first": "Trayan",
         "last": "Iliev",
         "address": {
@@ -21,6 +22,7 @@ sample_contacts = [
         ]
     },
     {
+        "id": 2,
         "first": "Georgi",
         "last": "Spasov",
         "address": {
@@ -41,6 +43,18 @@ sample_contacts = [
     }
 ]
 
+def format_contact(contact: dict) -> str:
+    result = ''
+    phones_str = ', '.join([f'{ph["type"]}: {ph["number"]}' for ph in contact['phones']])
+    result += f'| {contact["id"]:>3d} | ' \
+              f'{contact["first"] + " " + contact["last"]:<15.15s} | ' \
+              f'{contact["address"]["street"] + ", " + contact["address"]["city"] + ", " + contact["address"]["country"]:<40.40s} | ' \
+              f'{phones_str:40.40s} |'
+    return result
+
+def print_contacts(contacts):
+    for contact in contacts:
+        print(format_contact(contact))
 
 if __name__ == "__main__":
     with open('contacts.json', 'wt', encoding='utf-8') as f:
@@ -48,4 +62,4 @@ if __name__ == "__main__":
 
     with open('contacts.json', 'rt', encoding='utf-8') as f:
         contacts_list = json.load(f)
-        print(contacts_list)
+        print_contacts(contacts_list)
