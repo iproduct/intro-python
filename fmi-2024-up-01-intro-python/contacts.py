e67e21dce5e323351b07d9278aeeebf394ea09b9
Trayan Iliev <office@iproduct.org>
2024-02-08 11:15:18 +0200

    contacts demo - sample contacts added

---
+++ fmi-2024-up-01-intro-python/contacts.py
@@ -0,0 +1,44 @@
+sample_contacts = [
+    {
+        "first": "Trayan",
+        "last": "Iliev",
+        "address": {
+            "country": "BG",
+            "city": "Sofia",
+            "street": "Jamaes Bouchier Blvd., 42"
+        },
+        "phones": [
+            {
+                "type": "work",
+                "number": "+3592895423"
+            },
+            {
+                "type": "mobile",
+                "number": "+359885167243"
+            },
+        ]
+    },
+    {
+        "first": "Georgi",
+        "last": "Spasov",
+        "address": {
+            "country": "BG",
+            "city": "Plovdiv",
+            "street": "Pet tepeta, 18"
+        },
+        "phones": [
+            {
+                "type": "work",
+                "number": "+3594312345"
+            },
+            {
+                "type": "mobile",
+                "number": "+359889195572"
+            },
+        ]
+    }
+]
+
+
+if __name__ == "__main__":
+    pass
