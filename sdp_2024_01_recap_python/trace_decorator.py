6cb1580c5994b951eb3986f7ed3720bf971457a7
Trayan Iliev <office@iproduct.org>
2024-09-05 12:58:02 +0300

    custom decorators added

---
+++ sdp_2024_01_recap_python/trace_decorator.py
@@ -0,0 +1,37 @@
+from time import time_ns
+
+
+def trace(func):
+    def wrapper(*args, **kwargs):
+        print(f'{func.__name__} is called with {args}, {kwargs}.')
+        result = func(*args, **kwargs)
+        print(f'{func.__name__} has returned the result: {result}')
+        return result
+    return wrapper
+
+def profile(func):
+    def wrapper(*args, **kwargs):
+        print(f'{func.__name__} is called with {args}, {kwargs}.')
+        start = time_ns()
+        result = func(*args, **kwargs)
+        end = time_ns()
+        print(f'{func.__name__} has returned after {end - start} ns, with the result: {result}')
+        return result
+    return wrapper
+
+@profile
+def fn1(x, y):
+    z= 0
+    for i in range(1000000):
+        z+=1
+    return x + y
+
+@profile
+def fn2(x, y):
+    z = 0
+    for i in range(100000):
+        z += 1
+    return x * y
+
+if __name__ == '__main__':
+    fn2(fn1(fn2(3,5), 2), 4)
