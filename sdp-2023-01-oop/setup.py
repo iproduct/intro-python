fe57a5f7866219772e994f17a5ce8db1ae3068e6
Trayan Iliev <office@iproduct.org>
2024-09-06 09:54:16 +0300

    requirements.txt added

---
+++ sdp-2023-01-oop/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sdp-2023-01-oop',
+    version='',
+    packages=['dao', 'model', 'business', 'exceptions', 'presentation'],
+    url='',
+    license='',
+    author='office27',
+    author_email='',
+    description=''
+)
