a0c3a837dc83177f6710ffa1f60770128e44b272
Trayan Iliev <office@iproduct.org>
2024-09-09 18:40:26 +0300

    find path using recursion - find first path complete

--- sdp_2024_03_algorithms/labyrinth.py
+++ sdp_2024_03_algorithms/labyrinth.py
@@ -17,8 +17,10 @@ class Labyrinth:
         return self.__rows[row][col] == '_'
 
     def draw_path(self, path: list[tuple[int, int]]):
+        draw_result = self.__rows[:]
         for i,(x,y) in enumerate(path):
-            self.__rows[y] = self.__rows[y][:x] + str(i%10) + self.__rows[y][x+1:]
+            draw_result[y] = draw_result[y][:x] + str(i%10) + draw_result[y][x+1:]
+        return '\n'.join(draw_result)
 
     def load(self, filename):
         with open(filename, 'r') as f:
@@ -39,6 +41,7 @@ class FindPath:
     def __init__(self, labyrinth: Labyrinth):
         self.labyrinth = labyrinth
         self.__visited = set()
+        self.__shortest_path = None
 
     def find_path(self,  start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
         # print(start, end, self.__visited)
@@ -54,6 +57,25 @@ class FindPath:
                     # print(f'Path: {pth}')
                     return pth
         return None
+
+    def find_paths(self,  start: tuple[int, int], end: tuple[int, int]) -> list[list[tuple[int, int]]]:
+        # print(start, end, self.__visited)
+        if start == end:
+            # print(f'Path: {[start]}')
+            return [[start]]
+        result_paths = []
+        for neighbour in self.get_free_neighbours(start):
+            if neighbour not in self.__visited:
+                self.__visited.add(start)
+                # print(f'Neighbour: {neighbour}, visited: {self.__visited}')
+                paths = self.find_paths(neighbour, end)
+                self.__visited.remove(start)
+                for p in paths:
+                    p.insert(0, start)
+                    result_paths.append(p)
+                    # print(f'Path: {pth}')
+
+        return result_paths
 
     def get_free_neighbours(self, start: tuple[int, int]) -> list[tuple[int, int]]:
         neighbours = []
@@ -72,12 +94,16 @@ if __name__ == '__main__':
     # print(lab)
     # lab.save('labyrinth01.txt')
     lab = Labyrinth()
-    lab.load('labyrinth01.txt')
+    lab.load('labyrinth02.txt')
     print(lab)
     find_path = FindPath(lab)
     # cell = (4,4)
     # print(f'Free neighbours of {cell} are: {find_path.get_free_neighbours(cell)}')
-    path = find_path.find_path((0, 0), (lab.width - 1, lab.height - 1))
-    print(path)
-    lab.draw_path(path)
-    print(lab)
+    paths = find_path.find_paths((0, 0), (lab.width - 1, lab.height - 1))
+    for p in paths:
+        print(p)
+        print(lab.draw_path(p))
+
+    paths.sort(key=lambda path: len(path))
+    for p in paths:
+        print(p)
