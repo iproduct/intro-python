cdfd9621827cc2b405afe9f7094162e4764725da
Trayan Iliev <office@iproduct.org>
2024-09-11 18:33:52 +0300

    quicksort using Hoare impl. complete

--- sdp_2024_03_algorithms/sorting.py
+++ sdp_2024_03_algorithms/sorting.py
@@ -33,11 +33,44 @@ def sort_insertion(a: list[int]):
         a[j + 1] = key
 
 
+def quicksort(a: list[int]):
+    quicksort_rec(a, 0, len(a))
+
+def quicksort_rec(a: list[int], left, right):
+    # print(f'{left}, {right} :{a}')
+    if left >= right - 1: #recursion bottom
+        return
+
+    pivot = a[left] # recursion step
+    i = left
+    j = right
+    while i <= j:
+        while True:
+            i += 1
+            if i >= j or a[i] >= pivot :
+                break
+        while True:
+            j -= 1
+            if i >= j or a[j] <= pivot :
+                break
+        # print(f'i={i}, j={j}')
+        if i < j:
+            a[i], a[j] = a[j], a[i]
+    # print(f'Swap: i={i}, j={j}')
+    a[j], a[left] = a[left], a[j]
+    # print(f'After: {a[left: right]}, {left}, {right}, Pivot = {pivot} -> {j}')
+    quicksort_rec(a, left, j)
+    quicksort_rec(a, j+1, right)
+
+
+
+
+
 if __name__ == '__main__':
-    # a = []
-    # for i in range(100):
-    #     a.append(randint(1, 100))
-    a = [73, 4, 45, 39, 86, 82, 7, 54, 94, 91, 16, 59, 74, 73, 39, 35, 69, 62, 31, 98, 14, 57, 75, 27, 26, 11, 24, 20, 18, 77, 27, 58, 74, 64, 77, 10, 1, 11, 100, 100, 15, 11, 62, 67, 40, 11, 54, 73, 32, 73, 69, 19, 67, 86, 24, 34, 69, 84, 72, 37, 80, 61, 9, 68, 83, 27, 45, 60, 29, 94, 38, 69, 78, 9, 69, 94, 78, 73, 63, 5, 83, 3, 68, 81, 26, 17, 89, 50, 96, 100, 13, 32, 68, 44, 27, 74, 14, 79, 15, 73]
+    a = []
+    for i in range(100):
+        a.append(randint(1, 100))
+    # a = [73, 4, 45, 39, 86, 82, 7, 54, 94, 91, 16, 59, 74, 73, 39, 35, 69, 62, 31, 98, 14, 57, 75, 27, 26, 11, 24, 20, 18, 77, 27, 58, 74, 64, 77, 10, 1, 11, 100, 100, 15, 11, 62, 67, 40, 11, 54, 73, 32, 73, 69, 19, 67, 86, 24, 34, 69, 84, 72, 37, 80, 61, 9, 68, 83, 27, 45, 60, 29, 94, 38, 69, 78, 9, 69, 94, 78, 73, 63, 5, 83, 3, 68, 81, 26, 17, 89, 50, 96, 100, 13, 32, 68, 44, 27, 74, 14, 79, 15, 73]
     print(a)
-    sort_insertion(a)
+    quicksort(a)
     print(a)
