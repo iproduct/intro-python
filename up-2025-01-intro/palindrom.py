81911cd2aafcbd2aef6a04be3466317abdb3b44c
Trayan Iliev <office@iproduct.org>
2025-02-04 11:51:21 +0200

    palidrom demo added

--- up-2025-01-intro/palindrom.py
+++ up-2025-01-intro/palindrom.py
@@ -1,8 +1,12 @@
 def is_palindrome(s: str) -> bool:
-    # TODO implement predicate
+    s = s.lower().replace(' ', '')
+    i = 0
+    l = len(s) // 2
+    while i <= l and s[i] == s[-i-1]:
+        i += 1
+    if i > l:
+        return True
     return False
-
-
 
 if __name__=="__main__":
     print(is_palindrome('Able was I ere I saw Elba'))
