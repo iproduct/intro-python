8495c00fa7ecd775894e82beab12118231624a8f
Trayan Iliev <office@iproduct.org>
2025-02-05 13:04:30 +0200

    file demo added

---
+++ up-2025-01-intro/files.py
@@ -0,0 +1,20 @@
+from labirynth import print_labyrinth
+
+def read_labyrinth(filename: str) -> list[list[int]]:
+    # f = open(filename, 'rt')
+    labyrinth = []
+    with open(filename, 'rt') as f:
+        lines = f.readlines()
+        rows, cols = lines[0].split(',')
+        for row in range(1, int(rows) + 1):
+            cells = lines[row].split(',')
+            lab_row = []
+            for cell in cells:
+                lab_row.append(int(cell))
+            labyrinth.append(lab_row)
+    return labyrinth
+
+
+if __name__ == "__main__":
+    labyrinth = read_labyrinth('labyrinth.txt')
+    print_labyrinth(labyrinth)
