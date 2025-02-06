f3ef9c7273bd461dc6a2daa96625a73b557f0722
Trayan Iliev <office@iproduct.org>
2025-02-06 10:49:14 +0200

    keywords demo developed

--- up-2025-01-intro/keywords.py
+++ up-2025-01-intro/keywords.py
@@ -1,9 +1,21 @@
 from collections.abc import Iterable
+import re
+
+def get_count(word_count: tuple[str, int]) -> int:
+    return word_count[1]
 
 def get_keywords(lines: Iterable[str], count = 100) -> list[tuple[str, int]]:
+    word_counts = {}
     for line in lines:
-        print(line)
-    return []
+        words = re.split('[^a-zA-Z_]+', line)
+        for word in words:
+            if len(word) < 2:
+                continue
+            word = word.lower()
+            word_counts[word] = word_counts.get(word, 0) + 1
+    keywords = list(word_counts.items())
+    keywords.sort(key = get_count, reverse = True)
+    return keywords[:count]
 
 if __name__ == '__main__':
     with open('article.txt', 'rt') as f:
