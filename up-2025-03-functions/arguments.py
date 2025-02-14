045aee40f9a44047511653745052b6165a67754a
Trayan Iliev <office@iproduct.org>
2025-02-14 09:02:41 +0200

    function args demo added

--- up-2025-03-functions/arguments.py
+++
@@ -1,7 +0,0 @@
-def f(x, y, /, z, *args, mode='read', **kwargs):
-    print(x, y, z, args, mode, kwargs)
-
-
-if __name__ == '__main__':
-    f(1, 2, z=3, 4, 5, 6, mode='write', color='red', highlight='green')
-    f(1, 2, z=3)
