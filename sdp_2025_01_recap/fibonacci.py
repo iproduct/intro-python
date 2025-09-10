ea2872c14cd7c25f7b584119cbe839b40ea649e1
Trayan Iliev <office@iproduct.org>
2025-09-10 13:14:50 +0300

    console logging added

--- sdp_2025_01_recap/fibonacci.py
+++ sdp_2025_01_recap/fibonacci.py
@@ -11,15 +11,18 @@ def fib(n):
     # elif n==1:
     #     return 1
     if n in memoize:
+        print(f"{n} is already fibonacci")
         return memoize[n]
     else:
+        print(f"Calling fib({n - 1})  and fib({n-2})")
         fib_n = fib(n-1) + fib(n-2)
         memoize[n] = fib_n
+        print(f'fib({n}) = {fib_n} ')
         return fib_n
 
 if __name__ == '__main__':
     sys.setrecursionlimit(2000)
     sys.set_int_max_str_digits(10000)
-    n = 1000
+    n = 10
     print(n, '->', fib(n))
     print(f'Number calls: {calls}')
