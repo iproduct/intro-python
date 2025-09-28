8222756fc52d893b60a5f7fdcad31e7792615c74
Trayan Iliev <office@iproduct.org>
2025-09-28 18:06:00 +0300

    bibary_search implemented

--- sdp_2025_03_alg_datastruct/sorting.py
+++ sdp_2025_03_alg_datastruct/sorting.py
@@ -1,3 +1,6 @@
+from unittest.mock import right
+
+
 def sort_bubble[T](arr: list[T]):
     swap = True
     i = 0
@@ -26,14 +29,15 @@ def sort_ins[T](arr: list[T]):
     for i in range(1, len(arr)):
         current = arr[i]
         j = i - 1
-        while  j >= 0 and current < arr[j]:
+        while j >= 0 and current < arr[j]:
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
             j -= 1
 
-def quick_sort[T](arr: list[T], start = 0, end = None):
+
+def quick_sort[T](arr: list[T], start=0, end=None):
     if end is None:
         end = len(arr)
-    if end - start <= 1: # recursion bottom
+    if end - start <= 1:  # recursion bottom
         return
     pivot = arr[end - 1]
     i = start
@@ -45,12 +49,14 @@ def quick_sort[T](arr: list[T], start = 0, end = None):
             j -= 1
         if i < j:
             arr[i], arr[j] = arr[j], arr[i]
-    arr[j+1], arr[end-1] = arr[end-1], arr[j+1]
-    quick_sort(arr, start, j+ 1)
-    quick_sort(arr, j+2, end)
+    arr[j + 1], arr[end - 1] = arr[end - 1], arr[j + 1]
+    quick_sort(arr, start, j + 1)
+    quick_sort(arr, j + 2, end)
 
 
 MAX_VALUE = 101
+
+
 def counting_sort[T](arr: list[T]):
     counts = [0] * MAX_VALUE
     for elem in arr:
@@ -61,8 +67,25 @@ def counting_sort[T](arr: list[T]):
             arr.append(i)
 
 
+def binary_search[T](arr: list[T], elem: T) -> int:
+    left = 0
+    right = len(arr) - 1
+    middle = (left + right) // 2
+    while left < right and arr[middle] != elem:
+        if arr[middle] < elem:
+            left = middle + 1
+        else:
+            right = middle - 1
+        middle = (left + right) // 2
+    if arr[middle] == elem:
+        return middle
+    return middle - len(arr)
+
+
 if __name__ == '__main__':
     a = [12, 46, 18, 9, 100, 92, 15, 9, 50, 40, 50, 20, 9, 12, 3, 50, 17, 100, 30, 7, 46, 9, 50, 70, 3, 1]
     # a = [9, 12, 46, 108, 92, 15, 150, 170]
-    counting_sort(a)
-    print(a)
+    quick_sort(a)
+    print(len(a), a)
+    print(f'17 in index: {binary_search(a, -5)}')
+
