d5db3bd77efa4e879f6ac71141e471cc48be622c
Trayan Iliev <office@iproduct.org>
2025-10-21 18:07:26 +0300

    model Contact complte

--- sdpr_2025_03_oop/model/contact.py
+++ sdpr_2025_03_oop/model/contact.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 class Contact:
     def __init__(self, first_name = '', last_name = '', phone_number = '', email = '', address = '',
                  created = None, modified = None):
