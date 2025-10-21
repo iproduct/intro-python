3b2c5066b6efaa180b8a15c699b8eb41b4caf25b
Trayan Iliev <office@iproduct.org>
2025-10-21 17:42:43 +0300

    model Contact

---
+++ sdpr_2025_03_oop/model/contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, first_name, last_name, phone_number, email, address, created, modified):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
+        self.created = created
+        self.modified = modified
