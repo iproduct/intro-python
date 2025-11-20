fa0b7dce10572e64ea2376dff12f11abc40a606d
Trayan Iliev <office@iproduct.org>
2025-11-20 13:59:03 +0200

    add, str, trsverse debugged and completed

--- sdpr_2025_05_sorting_trees_graphs/binary_tree_linked.py
+++ sdpr_2025_05_sorting_trees_graphs/binary_tree_linked.py
@@ -10,7 +10,7 @@ class BTNode[T]:
         self.left = left
         self.right = right
     def __str__(self):
-        return self.val
+        return str(self.val)
     def __repr__(self):
         return f'[{self.val}, P: {self.parent}, L: {self.left}, R: {self.right}]'
 
@@ -20,7 +20,7 @@ class BTNode[T]:
     def is_root(self):
         return self.parent is None
 
-ELEMENT_WIDTH = 6
+ELEMENT_WIDTH = 4
 
 class BinaryTreeLinked[T](BinaryTree[T]):
     def __init__(self):
@@ -30,13 +30,14 @@ class BinaryTreeLinked[T](BinaryTree[T]):
     def __str__(self):
         result = ''
         def visit_node(node: BTNode[T], depth: int, is_right: bool) -> bool:
+            # print('====>', repr(node), depth, is_right)
             nonlocal result
             if is_right:
-                result += f'{str(node.val).rjust(ELEMENT_WIDTH)}{"" if node.is_leaf() else "->"}'
+                result += f'{str(node.val).ljust(ELEMENT_WIDTH)}{"" if node.is_leaf() else "->"}'
             else:
                 if node.parent.right is None:
                     result += 'None'
-                result += f'\n{' ' * (ELEMENT_WIDTH + 2) * depth}{str(node.val).rjust(ELEMENT_WIDTH)}{"" if node.is_leaf() else "->"}'
+                result += f'\n{' ' * max((ELEMENT_WIDTH + 2) * depth - 2, 0)}->{str(node.val).ljust(ELEMENT_WIDTH)}{"" if node.is_leaf() else "->"}'
             return True
         self._traverse_dfs_pre_rtl(visit_node)
         return result
@@ -65,11 +66,11 @@ class BinaryTreeLinked[T](BinaryTree[T]):
             return None, False
         if root.val == val:
             return root, True
-        if root.val < val:
+        if val < root.val:
             if root.left is None:
                 return root, False
             return self._find(val, root.left)
-        if root.val > val:
+        if  val > root.val:
             if root.right is None:
                 return root, False
             return self._find(val, root.right)
@@ -98,7 +99,9 @@ class BinaryTreeLinked[T](BinaryTree[T]):
     def add(self, val: T):
         if self.root is None:
             self.root = BTNode(val)
+            return
         node, found = self._find(val)
+        # print('====>find: ', repr(node), found)
         if found:
             node.val = val
         else:
@@ -108,6 +111,7 @@ class BinaryTreeLinked[T](BinaryTree[T]):
                 node.left = new_node
             else:
                 node.right = new_node
+        # print('====>find- return: ', repr(node), found)
         self.size = self.size + 1
 
     def remove(self, node: T):
@@ -128,5 +132,19 @@ class BinaryTreeLinked[T](BinaryTree[T]):
     def iter_bfs(self) -> Iterator[T]:
         pass
 
+if __name__ == '__main__':
+    t = BinaryTreeLinked()
+    t.add(5)
+    t.add(10)
+    t.add(2)
+    t.add(1)
+    t.add(4)
+    t.add(42)
+    t.add(41)
+    t.add(11)
+    t.add(3)
+    t.add(9)
+
+    print(t)
 
 
