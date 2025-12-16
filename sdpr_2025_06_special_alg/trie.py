fde6532638fef538d03624be686980cd83f603b1
Trayan Iliev <office@iproduct.org>
2025-12-16 17:54:32 +0200

    trie implemented

--- sdpr_2025_06_special_alg/trie.py
+++ sdpr_2025_06_special_alg/trie.py
@@ -2,11 +2,20 @@ class TrieNode:
     def __init__(self):
         self.child = [None] * 26
         self.wordEnd = False
+    def __str__(self):
+        data = ' '.join([f"{chr(ord('a') + i)}{str(self.child[i])}"
+                  for i in range(len(self.child)) if self.child[i] is not None])
+        if len(data) > 0:
+            return '[' + data + ']'
+        return ''
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+
+    def __str__(self):
+        return str(self.root)
 
     def insert(self, word):
         current = self.root
@@ -26,3 +35,13 @@ class Trie:
             current = current.child[letter_index]
         return current.wordEnd
 
+if __name__ == '__main__':
+    words = ['and', 'ant', 'do', 'geek', 'dad', 'ball']
+    trie = Trie()
+    for word in words:
+        trie.insert(word)
+    print(trie)
+
+    search_keys = ['do', 'gee', 'bat', 'ball']
+    for word in search_keys:
+        print(word, ' --> ', trie.search(word))
