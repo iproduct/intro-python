32e0399c4bbfa1ad20fb5301cde289f3b49730e9
Trayan Iliev <office@iproduct.org>
2025-12-23 17:41:00 +0200

    LCS using dinamic programming

--- sdpr_2025_06_special_alg/max_common_sequence.py
+++ sdpr_2025_06_special_alg/max_common_sequence.py
@@ -1,21 +1,39 @@
-def lcs(strX, strY):
-    lenX = len(strX)
-    lenY = len(strY)
-    row = [0] * (lenX + 1)
+def lcs(str_x, str_y):
+    lenX = len(str_x)
+    lenY = len(str_y)
+    print(f'lenX:{lenX}, lenY:{lenY}')
     L = [[0] * (lenX + 1) for i in range(lenY + 1)]
     for i in range(lenY):
         for j in range(lenX):
-            if strX[j] == strY[i]:
+            if str_x[j] == str_y[i]:
                 L[i+1][j+1] = 1 + L[i][j]
             else:
                 L[i+1][j+1] = max(L[i][j + 1], L[i + 1][j])
-    print_matrix(L)
+    return L
+
+def LCS(str_x, str_y, L):
+    j, i = len(str_x), len(str_y)
+    solution = []
+    while L[i][j] > 0:
+        # print(f'X[{j-1}] = {str_x[j-1]}, Y[{i-1}] = {str_y[i-1]}, solution: {solution}')
+        if str_x[j - 1] == str_y[i - 1]:
+            solution.append(str_x[j - 1])
+            i -= 1
+            j -= 1
+        elif L[i -1][j] > L[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    return ''.join(reversed(solution))
 
 def print_matrix(L):
-    for i in range(len(L)):
-        for j in range(len(L[i])):
+    for j in range(len(L[0])):
+        for i in range(len(L)):
             print(L[i][j], end=' ')
         print()
 
 if __name__ == '__main__':
-    lcs('GTTATAAATGACA', 'AGAAATCTA')
+    L = lcs('GTTCCTAATA', 'CGATAATTGAGA')
+    print_matrix(L)
+    solution = LCS('GTTCCTAATA', 'CGATAATTGAGA', L)
+    print('LCS: ', solution)
