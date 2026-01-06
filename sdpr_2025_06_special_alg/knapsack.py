4a96360a0247286b6570accf4ea718a6f22b2e9d
Trayan Iliev <office@iproduct.org>
2026-01-06 18:02:57 +0200

    dices and knapsack problems problem solved

--- sdpr_2025_06_special_alg/knapsack.py
+++ sdpr_2025_06_special_alg/knapsack.py
@@ -14,8 +14,39 @@ def knapsack(W, weights, values, n):
     return dp[n][W]
 
 
-# Example Usage
-values = [60, 100, 120]
-weights = [10, 20, 30]
-capacity = 50
-print(knapsack(capacity, weights, values, len(values)))  # Output: 220
+
+
+def knapsack_backtracking(capacity, weights, profits):
+    max_profit = 0
+
+    def knapsack_backtrack(index, current_weight, current_profit):
+        nonlocal max_profit
+        n = len(values)
+
+        # Base case: reached end of items
+        if index == n:
+            if current_profit > max_profit:
+                max_profit = current_profit
+            return
+
+        # Option 1: Include item (if it fits)
+        if current_weight + weights[index] <= capacity:
+            knapsack_backtrack(index + 1, current_weight + weights[index], current_profit + values[index])
+
+        # Option 2: Exclude item
+        knapsack_backtrack(index + 1, current_weight, current_profit)
+
+    knapsack_backtrack(0,0,0)
+    return max_profit
+
+if __name__ == '__main__':
+    # Example Usage
+    # values = [60, 100, 120]
+    # weights = [10, 20, 30]
+    # capacity = 50
+    # Output: 220
+    values = [1,2,3]
+    weights = [4,5,1]
+    capacity = 4
+
+    print(knapsack_backtracking(capacity, weights, values))
