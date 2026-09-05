09b5c6778cc12b7783cb33f52c15b304f3b584e5
Trayan Iliev <office@iproduct.org>
2026-09-05 14:45:04 +0300

    oop demo user repository  added

--- fmi_sdp_2026_02_oop/dao/UserRepository.py
+++ fmi_sdp_2026_02_oop/dao/UserRepository.py
@@ -1,12 +1,27 @@
+from xml.dom.minidom import Entity
+
+from exception.non_exisiting_entity_exception import NonExistingEntityException
+
 class UserRepository:
     def __init__(self, id_generator):
         self.id_generator = id_generator
-        self.entities = {}
+        self.entities: dict[str, Entity] = {}
     def create(self, entity):
         entity.id = self.id_generator.generate_id()
         self.entities[entity.id] = entity
         return entity
     def update(self, entity):
-        if self.entities[entity.id] is None:
-            raise NonExistingEntityException(f'Entity {type(entity)}')
-            self.entities[entity.id] = entity
+        if entity.id not in self.entities:
+            raise NonExistingEntityException(f'Entity {type(entity)} with ID={entity.id} does not exist')
+        self.entities[entity.id] = entity
+        return entity
+    def find_by_id(self, entity_id):
+        if entity_id in self.entities:
+            return self.entities[entity_id]
+        return None
+    def find(self):
+        return self.entities.values()
+    def delete(self, entity_id):
+        if entity_id not in self.entities:
+            raise NonExistingEntityException(f'Entity with ID={entity_id} does not exist')
+        return self.entities.pop(entity_id)
