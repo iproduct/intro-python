146079ca20b28a9b61295292c8aa08081909bf08
Trayan Iliev <office@iproduct.org>
2026-09-07 19:19:37 +0300

    @abstractmethod decorator added to interface methods

--- fmi_sdp_2026_02_oop/dao/abstract_repository.py
+++ fmi_sdp_2026_02_oop/dao/abstract_repository.py
@@ -5,23 +5,29 @@ from dao.entity import Entity
 
 
 class AbstractRepository[IDType, EntityType](Protocol):
+    @abstractmethod
     def create(self, entity: EntityType):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     def update(self, entity: EntityType):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     def find_by_id(self, entity_id: IDType)-> EntityType | None:
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     def find(self)-> Iterable[EntityType]:
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     def delete(self, entity_id: IDType)-> EntityType:
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     def size(self) -> int:
-        ...
+        raise NotImplementedError
 
 
 
